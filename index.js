967e79f8e6c4b8688c5da344654ef36d6084b34f
Your Name <your.email@example.com>
2025-03-04 23:34:26 +0800

    style rock-paper-scissors UI

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const container = document.querySelector("#container");
 
+const div1 = document.createElement("div");
+const div2 = document.createElement("div");
+const div3 = document.createElement("div");
+
+const score1 = document.createElement("div");
+const score2 = document.createElement("div");
+const result = document.createElement("div");
+
 function getComputerChoice()
 {
     const choices = ['rock','paper','scissors'];
@@ -7,46 +15,44 @@ function getComputerChoice()
     return choices[random];
 }
 
-const getHumanChoice = () => {
+/*const getHumanChoice = () => {
     let choice = prompt('Enter your choice: ');
     return choice.toLowerCase();
-};
+}; */
 
 let humanScore = 0;
 let computerScore = 0;
-
 function Compare(player1, player2){
-    console.log(`Computer score: ${player2}`);
-    console.log(`Human score: ${player1}`);
 
-    if(player1 === player2){
-        return 'The result is a tie!';
-    }
-    else if(player1 > player2){
-        return 'You win!'; 
+    score1.textContent = `Human: ${humanScore}`;
+    score2.textContent = `Computer: ${computerScore}`;
+    
+    if(player1 == 5){
+        result.textContent = 'The human wins!'; 
     }    
-    else{
-        return 'You lose!';
+    else if (player2 == 5){
+        result.textContent = 'The computer wins!';
     }
 }
 
 function playRound(humanChoice, computerChoice){
+    div1.textContent = `You chose: ${humanChoice}`;
+    div2.textContent = `Computer chose: ${computerChoice}`;
 
-    console.log(`Computer chose: ${computerChoice}`);
     if (humanChoice === computerChoice){
-        return 'It is a tie!';
+        div3.textContent = 'It is a tie!';
     }
     switch(`${humanChoice}-${computerChoice}`){
         case 'rock-scissors':
         case 'paper-rock':
         case 'scissors-paper':
             humanScore++;
-            return 'You win!';
+            div3.textContent = 'You win!';
         case 'rock-paper':
         case 'paper-scissors':
         case 'scissors-rock':
             computerScore++;
-            return 'You lose!';
+            div3.textContent = 'You lose!';
     }
 }
 
@@ -55,6 +61,15 @@ buttons.forEach((button) => {
     button.addEventListener("click", () => {
         const human = button.id;
         const computer = getComputerChoice();
-        console.log(playRound(human,computer));
+        playRound(human,computer);
+        Compare(humanScore,computerScore);
     });
 });
+
+container.appendChild(div1);
+container.appendChild(div2);
+container.appendChild(div3);
+
+container.appendChild(score1);
+container.appendChild(score2);
+container.appendChild(result);
