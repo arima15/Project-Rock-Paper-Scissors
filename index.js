7b6b93698dc213460775ca6a65d74da46654b825
cheriemae20 <cheriemaecatubig21@gmail.com>
2025-03-03 22:02:57 -0800

    remove loop used for rounds

--- index.js
+++ index.js
@@ -48,11 +48,5 @@ function playRound(humanChoice, computerChoice){
     }
 }
 
-for(let i = 0; i < 5; i++){
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    console.log(playRound(humanChoice, computerChoice));
-}
-
 console.log();
 console.log(Compare(humanScore, computerScore));
